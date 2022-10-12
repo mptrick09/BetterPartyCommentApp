d89c82cdd57574da8c4a3b21627a820b9a0547ad
Munyangabo <munyangabo@hk.local>
2022-10-12 09:11:18 +0200

    Projektdateien hinzufügen.

---
+++ PartyCommentLibrary/Model/CommentModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PartyCommentLibrary.Model
+{
+    public class CommentModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string YourCity { get; set; }
+        public string YourComment { get; set; }
+       
+
+        public string PartyInfo
+        {
+            get { return $"Hi, I am {FirstName} {LastName} and I Live in {YourCity}. I would like to say that the Party was {YourComment}"; }              
+                    
+                    
+                    
+                    
+                     
+           
+        }
+
+
+
+    }
+}
---
+++ PartyConsoleUI/Program.cs
@@ -0,0 +1,90 @@
+﻿using PartyCommentLibrary.Model;
+using System;
+using System.Collections.Generic;
+
+namespace PartyConsoleUI
+{
+    class Program
+    {
+        private static List<CommentModel> comments = new List<CommentModel>();
+       public static void Main(string[] args)
+        {
+
+            GetCommentFromParty();
+            PrintPartyComment();
+
+
+
+
+        }
+
+
+
+
+        private static void PrintPartyComment()
+        {
+            foreach (CommentModel comment in comments)
+            {
+
+                Console.WriteLine($"Party Comment: {comment.PartyInfo}.");
+
+            }
+        }
+
+
+
+        private static void GetCommentFromParty()
+        {
+
+            string MoreComment = "";
+
+            do
+            {
+
+                CommentModel comment = new CommentModel();
+
+                
+                comment.FirstName = GetPartyCommentFromConsole("Enter your First Name");
+                
+                comment.LastName = GetPartyCommentFromConsole("Enter your Last Name");
+
+                
+                comment.YourCity = GetPartyCommentFromConsole("enter your City name");
+
+                
+                comment.YourComment = GetPartyCommentFromConsole("What is your Comment today about this Party");
+
+               
+                MoreComment = GetPartyCommentFromConsole("Do you want to give your Comment also (yes or No)");
+
+                comments.Add(comment);
+
+
+                Console.Clear();
+
+            } while (MoreComment.ToLower() == "yes");
+
+
+
+
+
+        }
+
+
+        private static string GetPartyCommentFromConsole(string message)
+        {
+            string output = "";
+            Console.WriteLine(message);
+            output = Console.ReadLine();
+            return output;
+
+        }
+    }
+}
+
+
+       
+        
+        
+        
+        
